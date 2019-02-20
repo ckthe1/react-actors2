9b34e13c861a6b652ad0544cb1a672caf5076be0
Ck T <chengkou@gmail.com>
2019-02-19 18:32:37 -0600

    clean up

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-
-
 class About extends Component {
     render() {
         return (
             <div className="App">
-                    <h2>About</h2> 
-                    <p>This organization only exist in my dream</p>
+                <h2>About</h2> 
+                <p>This organization only exist in my dream</p>
             </div>
         );
     }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+
 import './App.css';
 import Header from '../Header/Header';
 import About from '../About/About';
